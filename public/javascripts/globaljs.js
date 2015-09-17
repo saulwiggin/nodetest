76fff08ce17e79e1cf4a6b64a069f60d989526de
Saul Wiggin <saul.wiggin@encraft.co.uk>
2015-09-17 12:12:46 +0100

    add delete user

--- public/javascripts/globaljs.js
+++ public/javascripts/globaljs.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
 
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
     $('#btnAddUser').on('click', addUser);
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
 
 });
 
@@ -117,3 +119,42 @@ function addUser(event) {
         return false;
     }
 };
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
+
